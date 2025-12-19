29d1e6c851cfac7e860c5a043a9df14be73f97a8
Arvin <pontiff@Arvins-MacBook-Air.local>
2025-12-19 21:43:18 +0300

    New items

---
+++ simpleProgram/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    //This is my first program
+
+    /*
+    
+    Long comment too!
+    
+    */
+    printf("Hello worldly creatures!\n");
+    printf("Hope you're doing well in life!");
+    return 0;
+}
