aec67f2de97538e3830ec6d6c2ba5bab35f49d5d
Arvin <pontiff@Arvins-MacBook-Air.local>
2025-12-19 22:54:21 +0300

    New projects

---
+++ varTypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(){
+
+    char grade = 'F';
+    char name[] = "SOUL SISTER";
+    bool happy = true;
+
+    printf("Your grade is %c \n", grade);
+    printf("Your name is %s\n", name);
+    printf("%d", happy);
+    return 0;
+}
