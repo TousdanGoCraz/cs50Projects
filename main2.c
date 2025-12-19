7c1ea23bdb1ba6fa1ae6a7899509904f87e767d2
Arvin <pontiff@Arvins-MacBook-Air.local>
2025-12-19 22:53:57 +0300

    New projects

---
+++ main2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int age = 25;
+    float gpa = 3.52;
+    double pi = 3.14472141235;
+
+    printf("You are %d years old.",age);
+    printf("Your GPA is %.2f.", gpa);
+    printf("The value of pi is %lf", pi);
+    return 0;
+}
